3b45ff20427b17aa966136095d0dd500c973d1b5
farukh7 <frk10906@gmail.com>
2026-01-05 14:36:23 +0530

    Add Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import logo from "../assets/logo.png";
+
+const Navbar = () => {
+  return (
+    <div className="border-b border-gray-300 h-12 flex justify-between items-center px-10">
+      <img src={logo} alt="logo" className="h-5 w-5" />
+      <h2 className="font-semibold">Trello</h2>
+      <div></div>
+    </div>
+  );
+};
+
+export default Navbar;
